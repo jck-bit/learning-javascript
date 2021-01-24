2b2b488aef8ba2256dd1a83f3c5f49623adabf9d
jck-bit <59308363+jck-bit@users.noreply.github.com>
2021-01-24 23:47:19 +0300

    Add files via upload
    an array method()

--- learning.js
+++ learning.js
@@ -1,22 +1,30 @@
+//an aray method
 
-
-var str1 = ("this is a beautiful string");
-
-var index = str1.localeCompare("XYZ");
-document.write("localeCompare first :" + index);
-
-document.write("<br />");
-
-var index = str1.localeCompare( "AbCD ?" );
-document.write("localeCompare second:" + index);
-
-//match
-var str = "for more information,see chapter 3.4.5.1";
-
-var re = /(chapter \d+(\.\d)*)/i;
-var found = str.match( re );
-
-document.write(found);
-
-
-
+if (!Array.prototype.every) 
+{ 
+  Array.prototype.every = function(fun /*, thisp*/) 
+  { 
+    var len = this.length; 
+    if (typeof fun != "function") 
+      throw new TypeError(); 
+ 
+    var thisp = arguments[1]; 
+    for (var i = 0; i < len; i++) 
+    { 
+      if (i in this && 
+          !fun.call(thisp, this[i], i, this)) 
+        return false; 
+    } 
+ 
+    return true; 
+  }; 
+} 
+function isBigEnough(element, index, array) { 
+  return (element >= 10);
+  } 
+ 
+var passed = [12, 5, 8, 130, 44].every(isBigEnough)
+console.log("First Test Value : " + passed );  
+   
+passed = [12, 54, 18, 130, 44].every(isBigEnough); 
+console.log("Second Test Value : " + passed );  
