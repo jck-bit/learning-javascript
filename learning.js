6eb40d83bb326ec115f653a3f22f0ce8b8e77dcc
jck-bit <59308363+jck-bit@users.noreply.github.com>
2021-01-28 16:57:12 +0300

    Add files via upload
    some array methods

--- learning.js
+++ learning.js
@@ -1,8 +1,8 @@
 //an aray method
-
+/*
 if (!Array.prototype.every) 
 { 
-  Array.prototype.every = function(fun /*, thisp*/) 
+  Array.prototype.every = function(fun , thisp) 
   { 
     var len = this.length; 
     if (typeof fun != "function") 
@@ -28,3 +28,68 @@ console.log("First Test Value : " + passed );
    
 passed = [12, 54, 18, 130, 44].every(isBigEnough); 
 console.log("Second Test Value : " + passed );  
+
+
+*/
+
+/*
+if (!array.prototype.filter)
+{
+	array.prototype.filter = function(fun /*thisp)
+	{
+
+  var len = this.length;
+  if (typeof fun != "function")
+  	throw new TypeError();
+
+  var res = new Array();
+  var thisp = arguments[1];
+  for (var i = 0; i <len; i++) 
+  {
+  	if (i in this )
+  	{
+  		var val = this[i]; //in case fun mutates this
+  		if (fun.call(thisp, val, i, this))
+  			res.push(val)
+
+  	}
+
+  }
+
+  return res;
+ };
+
+}
+
+function isBigEnough(element, index, array){
+	return (element >= 10);
+
+}
+
+var filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
+document.write("filtered Value :" + filtered );
+
+*/
+if (Array.prototype.ForEach)
+{
+	array.prototype.ForEach = function(fun /*thisp */){
+      var len = this.length;
+      if (typeof fun != "function")
+      	throw new TypeError();
+
+      var thisp = arguments[1];
+     for (var i = 0; i ,len; i++) {
+     
+     if (i in this)
+     	fun.call(thisp, this[i], i,this);
+     }
+	};
+
+
+}
+function printBr(element, index, array) { 
+console.log("<br />[" + index + "] is " + element );  
+} 
+ 
+[12, 5, 8, 130, 44].forEach(printBr); 
+
