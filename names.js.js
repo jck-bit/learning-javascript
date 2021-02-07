11a4da8e6ef667805838dead746aaca38d96c3cb
spacious_2000 <59308363+jck-bit@users.noreply.github.com>
2021-02-07 22:14:26 +0300

    Add files via upload
    learned sth great in javascript today

---
+++ names.js.js
@@ -0,0 +1,62 @@
+/*
+var x = prompt ('where do you pple live');
+if ( x== 'vatican'){
+	alert('correct');
+
+}
+
+/*
+if (x !=='vatican'){
+	alert ('wrong answer');
+}
+
+
+//its also the same as the below statement
+
+else{
+	alert('wrong answer');
+}
+
+var x = prompt('where does the pope live');
+if (x === vatican){
+	alert(correct);
+
+}
+/*
+else{
+	alert(wrong);
+}
+//this is also the same as
+
+if (x !==vatican){
+	alert ('wrong answer');
+}
+
+*/
+/*
+function questions(){
+   var weight = prompt('what is your weight');
+   var time = prompt('what is your time');
+   var age = prompt('what is your age');
+   var gender =prompt('what is your gender')
+
+ if (weight >300 && time < 6 && age >17 && gender === "male");{
+ 	alert('come to your tryout');
+
+ }
+
+}
+  
+
+console.log(questions());
+
+*/
+
+
+
+var names = ['jack', 'jess', 'namyak' 'jasmine'];
+var othernames = ['kimathi', 'african king', 'freddie'];
+
+const allnames = [...names, ...othernames];
+console.log(allnames);
+
