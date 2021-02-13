3eabd0f676c352962fd21aeec6b85e6de916bb72
spacious_2000 <59308363+jck-bit@users.noreply.github.com>
2021-02-13 16:54:22 +0300

    Add files via upload
    a binary tree in javascript

--- ht.js
+++ ht.js
@@ -1,259 +1,136 @@
 
-/*ssss
-function remove_from_right(arr, n = -1){
-	return arr.slice(n)
-}
-console.log(remove_from_right ([1, 2, 3, 4], -1));
-
-//write a javascript programme to decapitalize the first letter of a string
-var str = 'Hey are you coming tommorow';
-console.log(str.toLocaleLowerCase());
-
-//to remove an element from the left of an array
-
-function remove_from_the_left(arr, n = 1){
-	return arr.slice(n)
-}
-console.log(remove_from_the_left([1, 2, 3, 4], 1));
-
-
-//a javascript  function to print to contents of the current window
-function print(){
-	window.print();
-}
-
-//a javascriptprogramme to get the date
-
-var dt = Date();
-console.log(dt);
-
-//find the area of a triangle
-var side1 = 5;
-var side2 = 6;
-var side3 = 7;
-
-var s = (side1 + side2 + side3)/2;
-
-var area = math.sqrt(s*((s-side1) *(s-side2) * (s-side3)))
-console.log(area);
-*/
-//how to create a calculator..for division and multiplication
-/*
-function multiplyby (){
-	num1 = document.getElementById("firstNumber").value;
-	num2 = document.getElementById("secondNumber").value;
-	document.getElementById("result").innerHTML = num1 * num2;
-}
-
-function divideby(){
-	num1 = document.getElementById("firstNumber").value;
-	num2 = document.getElementById("secondNumber").value;
-document.getElementById("result").innerHTML = num1 /num2;	
-}
-*/
-/*
- 
-var var_name = 'abcd'
- var n = 120;
-
- this[var_name] = n;
- console.log(this[var_name]);
-
-
-function difference(n){
-
-	if (n <= 13)
-		return 13 -n;
-
-	else
-		return(n -13) * 2;
-
-}
-
-console.log(difference(32))
-console.log(difference(11))
-*/
-
-// a js programme to check whether two numbers are 50 or their sum
-
-/*
-function test50(x, y){
-	return((x == 50 || y == 50) || (x + y == 50));
-
-}
- console.log(test50(50, 50));
- console.log(test50(40, 40));
- console.log(test50(20, 30));
- console.log(test50(40, 10));
-
- //to find the difference of 19 and its no.
-*/
-
-/*
-
-  function diff(n){
-  	if (n <= 19) {
-  		return (19-n)
-  	}
-  	else{
-  		return( n - 19) *3
-  	}
-  }
- console.log(diff(13));
- console.log(diff(29));
- */
- /*
- function positive_negative(x, y){ 
- 	if ((x > 0 && y > 0) ||x > 0 && y < 0  ){
- 		return true;
- 	}
-  else{
-   return false;
-  }
-
-  }
-
-  console.log(positive_negative(2,2));
-  console.log(positive_negative(-2, 2));
-  console.log(positive_negative(2, -2));
-
-
-  //for loop 
-  for (var i = 1; i <7; i++) {
-  console.log(i);
-}
-*/
-
-
-function start_java(str){
-	if (str.lenght < 4){
-		return false;
-	}
-
-   front = str.substring(0, 4);
-   if (front == 'java'){
-   	return true;
-   }
-
-   else{
-   	return false;
-   }
-}
-
-console.log(start_java('javascriptprogramme'));
-console.log(start_java('python'));
-console.log(start_java('java'));
-  
-
-function range(num){
-	if (num > 59.99) {
-		return false;
-	}
-	else{
-		return true;
+//binary search tree
+class Node {
+	constructor(data, left = null, right = null){
+		this.data = data;
+        this.left = left;
+        this.right = right; 
 	}
 }
 
-console.log(range(58  ))
-
-// a programme to check the greatest number
-
-function greatest_number(x, y, z)
-   {
-	max_val = 0;
-	if (x > y) 
-	{
-	    max_val = x;
+class BST {
+	constructor(){
+		this.root = null;
 	}
-	else{
-		max_val = y;
-	}
-	if (z >max_val) {
-		max_val = z;
-	}
-	return max_val;
-}
-console.log(greatest_number(10,2,225))
+	add(data){
+		const node = this.root;
+		if (node === null){
+			this.root = new Node(data);
+			return;
+		}
+		else {
+			const searchTree = function(node){
+				if (data < node.data){
+					if(node.left === null){
+						node.left = new Node(data);
+						return;
+					}
+					else if (node.left !== null) {
+						return searchTree(node.left);
+					}
+					else if (data > node.data) {
+						if (node.right === null) {
+							node.right = new Node(data);
+							return;
+						}
+						else if (node.right !== null) {
+							return searchTree(node.right);
+						}
 
+						else {
+							return null;
+						}
+					};
+					return searchTree(node);
+				}
 
-//creating a stack in javascript
- 
-var stack = function() {
-	this.count = 0;
-	this.storage = {};
-
-
-//adds a value at the end of the end of a stuck
-
-this.push = function(value){
-	this.storage[this.count] = value;
-	this.count++;
-
-}
-
-//removes and returns a value at the end of the stack
-
-this.pop = function(){
-	if (this.count === 0){
-		return undefined;
 			}
+			findmin()
+				let current = this.root;
+				while (current.left !==null){
+					current = current.left;
+				}
+				return current.data;
+			}
+			findmax()
+				let current = this.current;
+				while (current.right !== null)
+					current = current.right;
+				return current.data
+				}
+				
+			}
+			find (data)
+				let current = this.root;
+				while (current.data !== data){
+					if (data < current.data){
+						current = current.left;
+					}
+					else{
+						current = current.right;
+					}
+					if(current === null)
+						return null;
+					}
+					
+				
+				return current;
+			
+			ispresent(data) {
+				let current = this.root;
+				while (current){
+					if (data === current.data){
+						return true;
+					}
+					if (data < current.data){
+						current = current.left;
+					}	
+					else{
+						current = currrent.right;
+					}	
+				}
+				return false;
+			}
+		remove (data){
+			const removeNode = function(node, data){
+				if (node == null){
+					return null;
+				}
 
+				if(data == node.data){
+					//node has no children
+					if (node.left == null && node.right == null){
+						return null;
+					}
+					//node has no left child
+					if (node.left == null){
+						return node.right;
+					}
+					//node has no right child
+					if (node.right == null)
+						return node.left;
+				}
+			        //node has two children
+			        var tempNode = node.right;
+			        while (tempNode.left !== null){
+			        	tempNode =tempNode.left;
+			        }
+			        node.data = tempNode.data;
+			        node.right = removeNode(node.right, tempNode.data);
+			        return node;
+			}
+            
+              else if ( data <node.data) {
+              	node.left = removeNode(node.left,data);
+              	return node;
+              } 
+              else{
+              	node.right = removeNode(node.right, data);
+              	return node;
+              }
+			}
+			this.root = removeNode(this.root, data);
+	
 
-this.count--;
-var result = this.storage[this.count];
-delete this.storage[this.count];
-return result;
-
-}
- this.size = function(){
- 	return this.count;
- }
-
-//returns the value at the end of the stack
-this.peek = function(){
-	return this.storage[this.count-1];
-}
-
-}
-
-var mystack = new stack();
-
-mystack.push(2);
-mystack.push(2);
-console.log(mystack.peek());
-console.log(mystack.pop()); 
-console.log(mystack.peek());
-
-//a queue in javascript
-
-function Queue (){
-	collection = [];
-	this.print = function(){
-		console.log(collection);
-	};
-	this.enqueue = function(element){
-		collection.push(element);
-	};
-	this.dequeue = function(){
-		return collection.shift();
-	};
-	this.front = function(){
-		return collection[0];
-	};
-	this.size = function(){
-		return collection.length;
-	};
-	this.empty = function(){
-		return(collection.length === 0);
-	};
-
-}
-
-var q = new Queue();
-q.enqueue('a');
-q.enqueue('b');
-q.enqueue('c');
-q.print();
-q.dequeue();
-console.log(q.front());
-q.print();
+const bst = new BST();
+bst.add(4);
