436594298d8e1393fa2a5912ed0796c71be62b8a
spacious_2000 <59308363+jck-bit@users.noreply.github.com>
2021-02-21 21:54:51 +0300

    Add files via upload
    i have a bug i  failed to debug in javascript

--- no.js
+++ no.js
@@ -1,28 +1,32 @@
 
-for (var x = 0;  x <= 15; x++){
-	if (x == 0){
-		console.log('This is an even number');
+
+//another example of quicksort in javascript
+
+
+function quicksort(arr){
+	if (arr.length <= 1){
+		return arr;
 	}
-	else if ( x % 2 == 0) {
-		console.log (x +' even number');
+var arrlength = arr.length;
+var pivotposition = math.floor(arrlength /2);
+var pivotvalue = arr[pivotposition];
+var less = [];
+    more = [];
+    same = [];
+
+
+for(var i = 0; i <arrlength; i++){
+	if (arr[i] === pivotvalue){
+		same.push(arr[i]);
+	}
+	else if (arr[i] < pivotvalue) {
+		less.push(arr[i]);
+
 	}
 	else{
-		console.log(x +' is odd');
-
+                more.push(arr[i]);
+        }
 	}
+        return quicksort(less).concat(same, quicksort(more));
+  }
 
-}
-
-/*
-for (var x=0; x<=15; x++) {
-        if (x === 0) {
-                console.log(x +  " is even");
-        }
-        else if (x % 2 === 0) {
-                console.log(x + " is even");   
-        }
-        else {
-                console.log(x + " is odd");
-        }
-}
-*/
