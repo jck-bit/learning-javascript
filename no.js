999a2d82f99bea132b8c8a9b642047b88c545d33
spacious_2000 <59308363+jck-bit@users.noreply.github.com>
2021-02-21 17:06:30 +0300

    Add files via upload
    wow a great question i solved

--- no.js
+++ no.js
@@ -1,8 +1,28 @@
 
-// the second recursive function
- function sumrange(num){
- 	if(num === 1) return 1;
- 	return num + sumrange (num - 1);
-} 
+for (var x = 0;  x <= 15; x++){
+	if (x == 0){
+		console.log('This is an even number');
+	}
+	else if ( x % 2 == 0) {
+		console.log (x +' even number');
+	}
+	else{
+		console.log(x +' is odd');
 
-sumrange(5)
+	}
+
+}
+
+/*
+for (var x=0; x<=15; x++) {
+        if (x === 0) {
+                console.log(x +  " is even");
+        }
+        else if (x % 2 === 0) {
+                console.log(x + " is even");   
+        }
+        else {
+                console.log(x + " is odd");
+        }
+}
+*/
