c735219fbb008a2bf169054ca120747bc1e07e99
spacious_2000 <59308363+jck-bit@users.noreply.github.com>
2021-03-01 22:30:27 +0300

    Add files via upload
    just did two questions on the net

--- learning.js
+++ learning.js
@@ -1,23 +1,17 @@
-// A javascript programme to find the greatest common divisor(GCD)
-
- var gcd = function(a ,b){
-	if(! b){
-		return a;
-	}
-	return gcd(b, a % b);
+//Extract out the values at the specified indexes from a specified 
+function pull_at_index(arr,pullarr){
+	let removed = [];
+	let pulled = arr
+	 .map((v ,i) => (pullarr.includes(i) ? removed.push(v) :v))
+	 .filter((v,i) => !pullarr.includes(i));
+arr.length = 0;
+pulled.forEach(v => {
+	arr.push(v)
+});
+return removed;
 };
-console.log(gcd(390, 640));
-
-//finding a fibonacci sequence... Each subsequent number is the sum of the previous two
-var fibonacci = function(n) {
-	if (n ===1){
-		return[0, 1];
-	}
-	else{
-		var s = fibonacci( n-1);
-		s.push(s[s.length -1] + s[s.length -2]);
-		return s;
-	}
-};
-
-console.log (fibonacci(8));
+let arra1 = ['a', 'b', 'c', 'd', 'e', 'f'];
+console.log(pull_at_index(arra1, [1, 3]));
+let arra2 =  [1, 2, 3, 4, 5, 6, 7];
+console.log(pull_at_index(arra2, [4]));
+	
