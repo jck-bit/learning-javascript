23c88d4191078edd2f652921f8db0d6eb828331b
jck-bit <jckkinyanjui@gmail.com>
2021-04-21 23:07:02 +0300

    DOM operator

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-var foo = "bar";
-console.log(foo);
+var thisvar = 'first value';
+var thatvar = 'second value';
+
+console.log("thisvar:", thisvar,"andthatvar:",thatvar);
+
+//thisvar:first value and thatvar second value
+
+var greet ="hello", who = "world";
+console.log("%s, %s" , greet, who);
+
+var market = "am goning", today = "now";
+console.log("%s,%s",market,today);
+
+
+//using the DOM API
+
+var element = document.createElement('p');
+element.textContent = "hello world";
+document.body.appendChild(element);//add the newly created element to the DOM
